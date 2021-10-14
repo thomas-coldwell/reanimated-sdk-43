3d898f74935a4e8f142273a0faaf84367ff2d313
Thomas Coldwell <tcoldwell72@gmail.com>
2021-10-14 16:13:19 +0100

    Configure EAS Build

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
 
 export default function App() {
+  const val = useSharedValue(0.0);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
